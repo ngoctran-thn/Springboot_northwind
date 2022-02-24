9c002b4cabbbfe48c65bb457a5f9499376cd8ffb
Ngoc Tran <tranthainhien1603@gmail.com>
2022-02-24 21:50:41 +0200

    Entity - Order detail status

---
+++ src/main/java/com/ngoc/northwind/entities/OrderDetailsStatus.java
@@ -0,0 +1,43 @@
+package com.ngoc.northwind.entities;
+
+import javax.persistence.*;
+import java.util.LinkedHashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "order_details_status")
+public class OrderDetailsStatus {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Integer id;
+
+    @Column(name = "status_name", nullable = false, length = 50)
+    private String statusName;
+
+    @OneToMany(mappedBy = "orderDetailsStatus")
+    private Set<OrderDetail> orderDetails = new LinkedHashSet<>();
+
+    public Set<OrderDetail> getOrderDetails() {
+        return orderDetails;
+    }
+
+    public void setOrderDetails(Set<OrderDetail> orderDetails) {
+        this.orderDetails = orderDetails;
+    }
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+}
