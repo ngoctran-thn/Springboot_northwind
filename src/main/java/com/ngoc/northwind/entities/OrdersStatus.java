e84cb467a19c05088237b220823905fecb3aba9f
Ngoc Tran <tranthainhien1603@gmail.com>
2022-02-24 21:51:49 +0200

    Entity - order status

---
+++ src/main/java/com/ngoc/northwind/entities/OrdersStatus.java
@@ -0,0 +1,33 @@
+package com.ngoc.northwind.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "orders_status")
+public class OrdersStatus {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Boolean id = false;
+
+    @Column(name = "status_name", nullable = false, length = 50)
+    private String statusName;
+
+    public String getStatusName() {
+        return statusName;
+    }
+
+    public void setStatusName(String statusName) {
+        this.statusName = statusName;
+    }
+
+    public Boolean getId() {
+        return id;
+    }
+
+    public void setId(Boolean id) {
+        this.id = id;
+    }
+}
